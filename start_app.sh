7cb6e23607a70608b1190f8736d4ffe48696c786
Panayot Panayotov <panayot.panayotov@movedigital.ch>
2022-09-15 21:21:00 +0300

    Add easier way to run tests in project

--- start_app.sh
+++ start_app.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+env="$1"
+
+if [ -z "$env" ]; then
+    env="dev"
+fi
+
+echo "Start '$env' environment"
+
 mkdir -p storage
 
 docker compose \
-    -f docker-compose.yml \
+    -f docker-compose.$env.yml \
     up \
     --build \
     --remove-orphans
